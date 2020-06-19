fcac4fa4f18bb8cddd085fd4b63845ba7c6529c0
Luis Pena <Luis Pena>
2020-06-19 03:28:00 -0400

    fixing the pokemons details

--- assets/scrypts.js
+++ assets/scrypts.js
@@ -5,12 +5,11 @@ var endpoint = " ";
 /*INDEX MAGAZINE*/
 /*============================================================================*/
 function fetchKantoPokemon(){
-    let endpoint = "ability?limit=12";
+    let endpoint = "pokemon?limit=12";
     fetch(poke_url+endpoint)
     .then(response => response.json())
     .then (function(allpokemon){
         allpokemon.results.forEach (function (pokemon) {
-            console.log(pokemon);
             magazine_col_I(pokemon);
         })
        })
@@ -29,32 +28,43 @@ function magazine_col_I(pokemon) {
         let cardHeader= document.createElement('div');
         let cardBody= document.createElement('div');
         let cardText= document.createElement('p');
+        let cardText_I= document.createElement('p');
+
         /*===================================================*/
         col.setAttribute("class", "col-xl-2");
         Pkimg.setAttribute("class", "card-img-top")
-        card.setAttribute("class", "card")
+        card.setAttribute("class", "card align-items-xl-stretch")
         cardHeader.setAttribute("class", "card-header")
         cardBody.setAttribute("class", "card-body")
         cardText.setAttribute("class", "card-text container")
+        cardText_I.setAttribute("class", "card-text container")
+
         
         /*===================================================*/
+
         col.appendChild(card);
         card.appendChild(cardHeader);
         card.appendChild(cardBody);
         cardBody.appendChild(Pkimg);
         card.appendChild(cardText);
-        console.log(poke_url+endpoint+pokeData.id);
+        card.appendChild(cardText_I);
+
         cardHeader.textContent = pokeData.name;
-        cardText.textContent = `Details: ${pokeData.effect_entries[0].effect}`;
+        fetch(pokeData.abilities[0].ability.url)
+        .then (response  => response.json ())
+        .then (function (pokeAbilities) {    
+        cardText.textContent = `Details: ${pokeAbilities.effect_entries[0].effect}`;
+        cardText_I.textContent = `Details: ${pokeAbilities.effect_entries[1].effect}`;
+               
+        })
         Pkimg.srcset = `https://pokeres.bastionbot.org/images/pokemon/${pokeData.id}.png`;
         
        /*===================================================*/
         magazine_col_I.appendChild (col);
    
-        console.log(pokeData);
-
       })
     }
+
 document.addEventListener('DOMContentLoaded', fetchKantoPokemon, false )
 
 console.log("hola mundo");
